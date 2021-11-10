a3b05675be23f4ae66d39d6621f8618d598eddc4
ankit <ankitgit21@gmail.com>
2021-11-10 19:56:29 +0530

    removed secerts

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -1,17 +1,24 @@
 from pathlib import Path
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vmc(lwt5%huu3_oog0onk4t#n*01l=1br==o))#0_g3-w)kh%v'
+SECRET_KEY =  env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
